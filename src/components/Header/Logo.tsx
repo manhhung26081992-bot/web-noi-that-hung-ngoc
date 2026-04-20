0ab6b151aeaa14c17718832464e244c855eb6e1c
manhhung26081992 <manhhung26081992@gmail.com>
2026-04-20 17:52:06 +0700

    updata: logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -7,7 +7,7 @@ export default function Logo() {
   return (
     <Link href="/" title="Nội Thất Hùng Ngọc - Bàn ghế văn phòng chất lượng cao">
       <div className={styles.logoContainer}>
-        <Image src="/logo1.png" alt="Logo Nội Thất Hùng Ngọc" width={70} height={70} priority />
+        <Image src="/logo.png" alt="Logo Nội Thất Hùng Ngọc" width={70} height={70} priority />
         <div className={styles.brandText}>
           <p className={styles.subName}>HÙNG NGỌC</p>
           <p className={styles.mainName}>furniture</p>

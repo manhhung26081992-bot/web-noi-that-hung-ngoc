a08022e1ae55699aa0bd1c5da458393c881d4ab5
manhhung26081992 <manhhung26081992@gmail.com>
2026-04-22 18:05:03 +0700

    update : layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,11 @@ export const metadata: Metadata = {
   },
   description: 'Chuyên cung cấp bàn ghế văn phòng, sofa giá rẻ, bền đẹp tại Hà Nội. Thiết kế thi công nội thất trọn gói, uy tín, giao hàng nhanh trong 24h.',
   keywords: ['nội thất hùng ngọc', 'bàn làm việc hà nội', 'ghế văn phòng giá rẻ', 'sofa phòng khách', 'thi công nội thất văn phòng'],
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/icon.png',
+    apple: '/icon.png',
+  },
   verification: {
         google:"8sRs-CQPkINftAG1gd1zSkOtsIGquewoAeFpp7L0LM4", // Dán chính xác mã trong ngoặc kép ở ảnh trước của bạn vào đây
     },

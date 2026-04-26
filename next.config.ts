b44026dd85fb2b61f49f4e32328ad5f810d433ba
manhhung26081992 <manhhung26081992@gmail.com>
2026-04-26 14:02:36 +0700

    bo Image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* 1. Tối ưu hóa hình ảnh */
   images: {
+    unoptimized:true,
      remotePatterns: [
       {
         protocol: 'https',
